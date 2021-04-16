0f5ba9bbdd1283cfec5e774d6d2fbc765b31cf12
RedGuy12 <paul@reid-family.org>
2021-04-16 12:04:14 -0500

    just merge already

--- addon-api/common/Storage.js
+++ addon-api/common/Storage.js
@@ -23,6 +23,12 @@ export default class Storage extends Listenable {
     if (typeof storedID !== "string") {
       throw new Error("Scratch Addons exception: stored ID must be a string");
     }
+    if (storedID.length == 0) {
+      throw new Error("Scratch Addons exception: stored ID is empty");
+    }
+    if (storedID.indexOf("/") > -1) {
+      throw new Error("Scratch Addons exception: stored ID can not have /s");
+    }
     if (!["sync", "local", "cookie"].includes(mode)) {
       throw new Error("Scratch Addons exception: mode must be one of: sync, local, or cookie");
     }
